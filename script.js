1ab726c3660fc5ee119a23862e5b6485ea9b9ac0
mohit8233 <mohitbanna085@gmail.com>
2025-12-10 12:29:52 +0530

    slider

---
+++ script.js
@@ -0,0 +1,34 @@
+// img Auto slider
+
+  let img = document.getElementById("img")
+
+
+  let images = [
+    "https://picsum.photos/id/217/1000/500",
+     "https://picsum.photos/id/257/1000/500",
+      "https://picsum.photos/id/297/1000/500",
+       "https://picsum.photos/id/227/1000/500",
+        "https://picsum.photos/id/247/1000/500",
+       
+  ]
+
+
+  let count = 0
+
+  function show(){
+    img.src = images[count]
+    img.style.width = "100%";
+    img.style.backgroundPosition = "cover"
+  }
+
+  show()
+
+ 
+  setInterval( ()=>{
+     count++
+     if(count === images.length)
+        count = 0
+
+      show()
+
+  }, 3000)
