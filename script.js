e38bfb192418432a8cdacc6c23e86c8950806046
mohit8233 <mohitbanna085@gmail.com>
2025-12-10 13:42:24 +0530

    slider

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 // img Auto slider
 
-  let img = document.getElementById("img")
+let img = document.getElementById("img")
 
 
-  let images = [
-    "https://picsum.photos/id/217/1000/500",
-     "https://picsum.photos/id/257/1000/500",
-      "https://picsum.photos/id/297/1000/500",
-       "https://picsum.photos/id/227/1000/500",
-        "https://picsum.photos/id/247/1000/500",
-       
-  ]
+let images = [
+  "https://picsum.photos/id/217/1000/500",
+  "https://picsum.photos/id/257/1000/500",
+  "https://picsum.photos/id/297/1000/500",
+  "https://picsum.photos/id/227/1000/500",
+  "https://picsum.photos/id/247/1000/500",
+
+]
 
 
-  let count = 0
+let count = 0
 
-  function show(){
-    img.src = images[count]
-    img.style.width = "100%";
-     img.style.height= "100%";
-    img.style.backgroundPosition = "cover"
-  }
+function show() {
+  img.src = images[count]
+  img.style.width = "100%";
+  img.style.height = "100%";
+
+}
+
+show()
+
+
+setInterval(() => {
+  count++
+  if (count === images.length)
+    count = 0
 
   show()
 
- 
-  setInterval( ()=>{
-     count++
-     if(count === images.length)
-        count = 0
-
-      show()
-
-  }, 3000)
+}, 3000)
